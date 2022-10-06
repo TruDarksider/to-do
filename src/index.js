4645d6696c76ff67117ab31741c4c3719de1e0a5
Tyler Hopkin <tyler.hopkin12@gmail.com>
2022-10-06 13:45:37 -0500

    Created toDo class and appendToDOM function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 console.log('I have linked correctly');
+
+class toDo {
+    constructor(title, description, dueDate, priority){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}
+
+function appendToDOM(newDo) {
+let tdTitle = document.createElement('h1');
+tdTitle.textContent = newDo.title;
+let tdDescription = document.createElement('p');
+tdDescription.textContent = newDo.description;
+tdTitle.appendChild(tdDescription);
+let tdDD = document.createElement('p');
+tdDD.textContent = newDo.dueDate;
+tdTitle.appendChild(tdDD);
+let tdPriority = document.createElement('p');
+tdPriority.textContent = newDo.priority;
+tdTitle.appendChild(tdPriority);
+document.body.appendChild(tdTitle);
+}
+
+appendToDOM(new toDo('Example', 'This is an example ToDo item', '1/2/2023', 'Moderate'));
