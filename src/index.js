0977d3caca5760ac57e8c531310939dd3c22d9d1
Tyler Hopkin <tyler.hopkin12@gmail.com>
2022-10-10 13:26:04 -0500

    Added button with form logic to add new To Do and a delete To Do button

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-console.log('I have linked correctly');
+import './style.css';
 
 class toDo {
     constructor(title, description, dueDate, priority){
@@ -21,7 +21,72 @@ tdTitle.appendChild(tdDD);
 let tdPriority = document.createElement('p');
 tdPriority.textContent = newDo.priority;
 tdTitle.appendChild(tdPriority);
+let trashButton = document.createElement('button');
+trashButton.classList.add('trash');
+trashButton.textContent = 'Delete';
+tdTitle.appendChild(trashButton);
+tdTitle.addEventListener('click', deleteToDo);
 document.body.appendChild(tdTitle);
 }
 
+function addNewToDo(){
+    const form = document.createElement('form');
+    const inTitle = document.createElement('input');
+    inTitle.setAttribute('type', 'text');
+    form.appendChild(inTitle);
+    const inDescription = document.createElement('input');
+    inDescription.setAttribute('type', 'text');
+    form.appendChild(inDescription);
+    const inDueDate = document.createElement('input');
+    inDueDate.setAttribute('type', 'text'); //FIND CALENDAR OR DATE VALUE FOR THIS INPUT
+    form.appendChild(inDueDate);
+    const inPriority = document.createElement('input');
+    inPriority.setAttribute('type', 'checkbox');
+    form.appendChild(inPriority);
+    const confirmBtn = document.createElement('input');
+    confirmBtn.setAttribute('type', 'button');
+    confirmBtn.setAttribute('value', 'Add To Do');
+    confirmBtn.classList.add('confirmAdd');
+    confirmBtn.addEventListener('click', (e)=>{
+        if(e.target.classList.contains('confirmAdd')){
+        appendToDOM(new toDo(inTitle.value, inDescription.value, inDueDate.value, inPriority.checked));
+        while(document.querySelector('.formArea').firstChild){document.querySelector('.formArea').firstChild.remove();}
+        }
+    });
+    form.appendChild(confirmBtn);
+    document.querySelector('.formArea').appendChild(form);
+}
+
+function deleteToDo(event) {
+    if(event.target.classList.contains('trash')){
+        event.target.parentNode.remove();
+        /*
+        let trashBook = event.target.parentNode.firstChild.textContent;
+        let trashIndex;
+        for(const book of myLibrary){
+            if(book.title == trashBook){
+                trashIndex = book.index;
+            }
+        }
+        myLibrary.splice(trashIndex, 1);
+        createCard();
+        */
+    }
+}
+
+function initializeDOM(){
+    let addButton = document.createElement('button');
+    addButton.classList.add('newToDo');
+    addButton.textContent = 'Add a new To Do';
+    let formArea = document.createElement('div');
+    formArea.classList.add('formArea');
+    document.body.appendChild(formArea);
+    document.body.appendChild(addButton);
+
+    addButton.addEventListener('click', ()=>{
+        if(!formArea.firstChild){addNewToDo();}
+    })
+}
+
+initializeDOM();
 appendToDOM(new toDo('Example', 'This is an example ToDo item', '1/2/2023', 'Moderate'));
